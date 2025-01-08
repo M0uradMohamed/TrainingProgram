45325c3cef8ae63c23411a6b45ede4dc97262372
M0uradMohamed <mouradmoahmed@gmail.com>
2025-01-08 20:11:32 +0200

    Instructor class

---
+++ Models/Instructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Instructor
+    {
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public DateOnly HiringDate { get; set; }
+        public string? MajorDegree { get; set; }
+        public DateOnly MajorDegreeDate { get; set; }
+        public string? Email { get; set; }
+        public string? OtherPhoneNumber { get; set; }
+    }
+}
