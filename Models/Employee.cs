90768fb06110974fe0023b24c944ac3f2bbc4ca6
M0uradMohamed <mouradmoahmed@gmail.com>
2025-01-08 17:03:35 +0200

    n-tier architecture

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? TraineeId { get; set; }
+        public string? Gender { get; set; }
+        public string? Major { get; set; }
+    }
+}
--- TrainingProgram/Program.cs
+++ TrainingProgram/Program.cs
@@ -1,3 +1,6 @@
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 namespace TrainingProgram
 {
     public class Program
@@ -8,6 +11,10 @@ namespace TrainingProgram
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options
+.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
