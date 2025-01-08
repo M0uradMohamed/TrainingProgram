97ebd03aa78a1259453600d0d03937f5e3c0141e
M0uradMohamed <mouradmoahmed@gmail.com>
2025-01-08 20:42:51 +0200

    errviewnodel right place

--- TrainingProgram/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TrainingProgram.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
