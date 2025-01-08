e16aca3f55d339232fe98d279c22050e5b773084
M0uradMohamed <mouradmoahmed@gmail.com>
2025-01-08 17:51:50 +0200

    employee class

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -9,9 +9,20 @@ namespace Models
     public class Employee
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
         public string? TraineeId { get; set; }
+        public string? Name { get; set; }
         public string? Gender { get; set; }
+        public string? Job { get; set; }
+        public string? Department { get; set; }
         public string? Major { get; set; }
+        public string? WorkPlace { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Belong  { get; set; }
+        public string? CompanyNameForForeign  { get; set; }
+
+
+        public int? SectorId { get; set; }
+        public int? DegreeId { get; set; }
+
     }
 }
